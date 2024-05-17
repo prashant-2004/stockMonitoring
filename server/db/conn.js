364ae9cf28f51049012552c3c8ecd189303ce0b7
prashant-2004 <prashant.kale0406@gmail.com>
2024-05-17 18:03:11 +0530

    modified conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE || "mongodb+srv://kaleprashant2004:MongoDB0406@services.irmjsdd.mongodb.net/test?retryWrites=true&w=majority&appName=services";
 
-mongoose.connect(DB, {
-    useNewUrlParser: true
-  }).then(() => {
+mongoose.connect(DB).then(() => {
     console.log('Connection Succesful');
   }).catch((err) => console.log('Connection failed'));
