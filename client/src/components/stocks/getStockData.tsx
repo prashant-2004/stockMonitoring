72b85c2f543827bb346f53ed5a5cccd122fcefb0
prashant-2004 <prashant.kale0406@gmail.com>
2024-05-17 17:52:56 +0530

    added vercel.json_2

--- client/src/components/stocks/getStockData.tsx
+++ client/src/components/stocks/getStockData.tsx
@@ -1,5 +1,5 @@
 export const getStockData = async (symbol:string) =>{
-    const apiKey = "UWNC92MVR8JO85DD";
+    const apiKey = process.env.ALPHA_VANTAGE_API_KEY || "UWNC92MVR8JO85DD";
     const symbol_2 = symbol;
     const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol_2}&interval=5min&apikey=${apiKey}`)
     const data = await response.json();
