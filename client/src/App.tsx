a7f805680ad4cf3981f242de0ee94fdbb3b56fa2
prashant-2004 <prashant.kale0406@gmail.com>
2024-05-18 02:34:58 +0530

    updated App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
-import RouteComponent from "./routes/RouteComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/users/Login";
+import Signup from "./pages/users/Signup";
+import Home from "./pages/users/Home";
+import Watchlist from './pages/users/Watchlist';
 
 function App() {
   return (
-    <RouteComponent/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/user-signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+      <Route path='/watchlist' element={<Watchlist/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
